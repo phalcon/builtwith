ca12b36706f5c04457e063ff4705fd2803c8bb1e
Ihor Sychevskyi <26163841+Arhell@users.noreply.github.com>
2019-03-01 14:35:30 +0200

    update for old browsers (#144)

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,30 +1,26 @@
-const client = algoliasearch(algoliaConfig.application_id, algoliaConfig.search_only_api_key);
-const index = client.initIndex(algoliaConfig.index_name);
+var client = algoliasearch(algoliaConfig.application_id, algoliaConfig.search_only_api_key);
+var index = client.initIndex(algoliaConfig.index_name);
 
-autocomplete('#searchinput', { hint: false }, [
-  {
-    source: autocomplete.sources.hits(index, { hitsPerPage: 5 }),
-    displayKey: 'name',
-    templates: {
-      suggestion(suggestion) {
-        return suggestion._highlightResult.title.value;
-      },
-    },
-  },
-]).on('autocomplete:selected', (event, suggestion, dataset) => {
-  window.location = suggestion.url
+autocomplete('#searchinput', { hint: false }, [{
+  source: autocomplete.sources.hits(index, { hitsPerPage: 5 }),
+  displayKey: 'name',
+  templates: {
+    suggestion: function suggestion(_suggestion) {
+      return _suggestion._highlightResult.title.value;
+    }
+  }
+}]).on('autocomplete:selected', function (event, suggestion, dataset) {
+  window.location = suggestion.url;
 });
 
-autocomplete('#searchinputmobile', { hint: false }, [
-  {
-    source: autocomplete.sources.hits(index, { hitsPerPage: 5 }),
-    displayKey: 'name',
-    templates: {
-      suggestion(suggestion) {
-        return suggestion._highlightResult.title.value;
-      },
-    },
-  },
-]).on('autocomplete:selected', (event, suggestion, dataset) => {
-  window.location = suggestion.url
+autocomplete('#searchinputmobile', { hint: false }, [{
+  source: autocomplete.sources.hits(index, { hitsPerPage: 5 }),
+  displayKey: 'name',
+  templates: {
+    suggestion: function suggestion(_suggestion2) {
+      return _suggestion2._highlightResult.title.value;
+    }
+  }
+}]).on('autocomplete:selected', function (event, suggestion, dataset) {
+  window.location = suggestion.url;
 });

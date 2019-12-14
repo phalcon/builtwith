122cabdf5968bca6394ab966ef0d4541ce58f2be
Nikolaos Dimopoulos <nikos@niden.net>
2019-12-14 18:35:32 -0500

    Update to the site

---
+++ updateData.php
@@ -0,0 +1,35 @@
+<?php
+
+$base      = "/opt/build/repo/";
+echo "Updating Sponsors" . PHP_EOL;
+
+$data = file_get_contents(
+    'https://raw.githubusercontent.com/phalcon/assets/master/phalcon/sponsors-fragment.html'
+);
+
+file_put_contents(
+    $base . '_includes/sponsors.html',
+    $data
+);
+
+echo "Updating Fan Art" . PHP_EOL;
+
+$data = file_get_contents(
+    'https://raw.githubusercontent.com/phalcon/assets/master/phalcon/fanart-fragment.html'
+);
+
+file_put_contents(
+    $base . '_includes/fanart.html',
+    $data
+);
+
+echo "Updating Footer" . PHP_EOL;
+
+$data = file_get_contents(
+    'https://raw.githubusercontent.com/phalcon/assets/master/phalcon/fanart-fragment.html'
+);
+
+file_put_contents(
+    $base . '_includes/footer.html',
+    $data
+);
